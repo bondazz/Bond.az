9354812a19ef7a9b414a8c03bba26bce8fcbd8b9
Samir Miriyev <samir.m@Mjfinance.local>
2026-05-20 11:59:37 +0400

    feat: add news-sitemap.xml back into sitemap.xml index

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -34,6 +34,10 @@ export async function GET() {
     <lastmod>${now}</lastmod>
   </sitemap>
   <sitemap>
+    <loc>${baseUrl}/news-sitemap.xml</loc>
+    <lastmod>${now}</lastmod>
+  </sitemap>
+  <sitemap>
     <loc>${baseUrl}/sitemaps/posts-az.xml</loc>
     <lastmod>${now}</lastmod>
   </sitemap>
